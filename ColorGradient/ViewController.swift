18448019851cf73c2b0646e1b1463b453ee42cb5
Jian Zhai <zhaijianjian@gmail.com>
2017-11-01 16:44:04 -0700

    Initial Commit

---
+++ ColorGradient/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ColorGradient
+//
+//  Created by Jian Zhai on 2017/11/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
